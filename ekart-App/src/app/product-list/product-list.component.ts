56a4f0416f02cdc3c2bb41740ba9150c0a729eed
abhishek <aksinghabhishek3155@gmail.com>
2024-10-20 21:23:46 +0530

    added product list component

--- ekart-App/src/app/product-list/product-list.component.ts
+++ ekart-App/src/app/product-list/product-list.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
 
+  productList = {
+    name: 'Iphone 16pro',
+    price: 5000,
+    color: 'Meta-Black',
+    discountPrice: 8.5,
+    Instock:5
+  };
+
+  getDiscount(){
+    return this.productList.price-(this.productList.price*this.productList.discountPrice/100);
+  }
 }
